650e598df83763b462d0c880a03690a1c2431b39
davidzhongsydney <david.zhong.uts@gmail.com>
2023-05-29 21:39:05 +1000

    init

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "ap-southeast-2"
+}
+
+resource "aws_instance" "myweb_server" {
+    ami = "ami-0ef66fb3631e10714"
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "HelloWorld"
+    }
+}
